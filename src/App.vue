a7aa2b3f36f12bfc5ba799fda68024b07a9e99c2
Aleksey Kondratov <zozzy@Zozzy-Mac.Dlink>
2019-08-02 01:03:46 +0300

    setTimeout issue fixed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="activityApp">
+  <div v-if="isDataLoaded" id="activityApp">
     <nav class="navbar is-white topNav">
       <div class="container">
         <div class="navbar-brand">
@@ -62,8 +62,8 @@ export default {
       isFetching: false,
       error: null,
       user: {},
-      activities: {},
-      categories: {}
+      activities: null,
+      categories: null
     }
   },
   computed: {
@@ -81,6 +81,9 @@ export default {
       } else {
         return 'No activities, so sad :('
       }
+    },
+    isDataLoaded () {
+      return this.activities && this.categories
     }
   },
   created () {
